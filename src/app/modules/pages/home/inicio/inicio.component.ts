b203ab6d046b0ddd944020c24903994824a357eb
VanerRay18 <vaneraymundo18@gmail.com>
2025-06-13 16:50:21 -0600

    inicio

--- src/app/modules/pages/home/inicio/inicio.component.ts
+++ src/app/modules/pages/home/inicio/inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,10 +8,68 @@ import { Router } from '@angular/router';
 })
 export class InicioComponent {
 
+  empresas = [
+    {
+      nombre: 'DHL',
+      entregados: 48,
+      faltantes: 12,
+      ruta: '/empresa/dhl'
+    },
+    {
+      nombre: 'J&T',
+      entregados: 22,
+      faltantes: 38,
+      ruta: '/empresa/jyt'
+    },
+    {
+      nombre: 'Mercado Libre',
+      entregados: 40,
+      faltantes: 20,
+      ruta: '/empresa/mercado-libre'
+    },
+    {
+      nombre: 'AMAZON',
+      entregados: 8,
+      faltantes: 52,
+      ruta: '/empresa/amazon'
+    }
+  ];
+
+  alertas = [
+    {
+      dias: 15,
+      mensaje: 'Sonia Juana González Niño ha solicitado vacaciones del 10/01/24 al 14/01/24 para Erik Randon Perez'
+    },
+    {
+      dias: 15,
+      mensaje: 'El contrato de Álvaro Clemente López Valderas finaliza el 19/01/2024'
+    },
+    {
+      dias: 15,
+      mensaje: 'Ernest Leon Hernández ha sido dado de alta'
+    }
+  ];
+
   constructor(private router: Router) {}
 
-  // Función para redirigir al usuario al dashboard
-  navigateToDashboard() {
-    this.router.navigate(['/dashboard']);
+  ngOnInit(): void {}
+
+  irAEmpresa(ruta: string) {
+    this.router.navigate([ruta]);
   }
+
+  irAsistencias() {
+    this.router.navigate(['/asistencias']);
+  }
+  getPorcentaje(empresa: any): number {
+  return (empresa.entregados / (empresa.entregados + empresa.faltantes)) * 100;
+}
+
+getColor(empresa: any): string {
+  const porcentaje = this.getPorcentaje(empresa);
+  if (porcentaje > 70) return 'bg-green-500';
+  if (porcentaje > 40) return 'bg-yellow-400';
+  return 'bg-red-600';
+}
+
 }
